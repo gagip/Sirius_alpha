cf0cf68a32ce8663f9afde9153ed0a7e38c3e165
SongJinBeom <cooo8@naver.com>
2019-07-11 21:48:55 +0900

    background prefabs

---
+++ Assets/01.Scripts/Interaction/TeleportButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TeleportButton : MonoBehaviour
+{
+    public string moveScene;    //이동할 씬
+    public Vector3 movePos;     //이동한 씬에서 캐릭터 위치
+    private GameObject Mary;
+    private GameObject teleportButton;
+    int i = 0;
+
+
+    void Start()
+    {
+        Mary = GameObject.FindGameObjectWithTag("Mary");
+        //movePos = new Vector3();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MainHall");
+        Mary.GetComponent<Transform>().position = movePos;
+    }
+
+
+    private void OnMouseDown()
+    {
+        ChangeScene();
+    }
+}
